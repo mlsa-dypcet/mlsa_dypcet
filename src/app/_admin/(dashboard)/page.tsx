d52336370b9b7ba405868145ba54300c92e22d1d
MLSA DYPCET CHAPTER <160157838+mlsa-dypcet@users.noreply.github.com>
2024-02-15 22:47:49 +0530

    Add files via upload

---
+++ src/app/_admin/(dashboard)/page.tsx
@@ -0,0 +1,18 @@
+// import { authOptions } from "@/server/auth/authOptions";
+// import { getServerSession } from "next-auth";
+// import { redirect } from "next/navigation";
+
+const Admin = async () => {
+
+  return (
+    <div>
+      {/* <pre>
+        <code>
+          <pre>{JSON.stringify(session, null, 2)}</pre>
+        </code>
+      </pre> */}
+    </div>
+  );
+};
+
+export default Admin;
